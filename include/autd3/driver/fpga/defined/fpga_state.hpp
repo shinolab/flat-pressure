7cede61eb482e5dabaa31401e5d9f4ac77ba0732
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-02-24 13:06:33 +0900

    update FPGAState

--- include/autd3/driver/fpga/defined/fpga_state.hpp
+++ include/autd3/driver/fpga/defined/fpga_state.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
+
+#include "autd3/native_methods.hpp"
 
 namespace autd3::driver {
 
@@ -16,7 +19,44 @@ class FPGAState {
   /**
    * @brief Check if thermal sensor is asserted
    */
-  AUTD3_API [[nodiscard]] bool is_thermal_assert() const noexcept { return (_info & 0x01) != 0; }
+  AUTD3_API [[nodiscard]] bool is_thermal_assert() const noexcept { return (_info & (1 << 0)) != 0; }
+
+  AUTD3_API [[nodiscard]] native_methods::Segment current_mod_segment() const noexcept {
+    switch (_info & (1 << 1)) {
+      case 0:
+        return native_methods::Segment::S0;
+      default:
+        return native_methods::Segment::S1;
+    }
+  }
+
+  AUTD3_API [[nodiscard]] std::optional<native_methods::Segment> current_stm_segment() const noexcept {
+    if (!is_stm_mode()) {
+      return std::nullopt;
+    }
+    switch (_info & (1 << 2)) {
+      case 0:
+        return native_methods::Segment::S0;
+      default:
+        return native_methods::Segment::S1;
+    }
+  }
+
+  AUTD3_API [[nodiscard]] std::optional<native_methods::Segment> current_gain_segment() const noexcept {
+    if (!is_gain_mode()) {
+      return std::nullopt;
+    }
+    switch (_info & (1 << 2)) {
+      case 0:
+        return native_methods::Segment::S0;
+      default:
+        return native_methods::Segment::S1;
+    }
+  }
+
+  AUTD3_API [[nodiscard]] bool is_gain_mode() const noexcept { return (_info & (1 << 3)) != 0; }
+
+  AUTD3_API [[nodiscard]] bool is_stm_mode() const noexcept { return !is_gain_mode(); }
 
   AUTD3_API [[nodiscard]] std::string to_string() const { return "Thermal assert = " + std::to_string(is_thermal_assert()); }
 
